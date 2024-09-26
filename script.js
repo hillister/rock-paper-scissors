b0a5b70124dcde286fc2fde528029bdbaf870af1
Ridhcheall Doherty <132768875+hillister@users.noreply.github.com>
2024-09-26 15:16:02 +0100

    added score keeping@
    q

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function getComputerchoice(){
+function getComputerChoice(){
     number = Math.floor((Math.random()) * 3) + 1
     if (number == 1){
         return `rock`;
@@ -26,30 +26,38 @@ let humanScore = 0
 let computerScore = 0
 
 function playRound(humanChoice, computerChoice){
-    humanChoice = getHumanChoice()
-    computerChoice = getComputerchoice()
-
     if (humanChoice == `rock` && computerChoice == `rock`){
         console.log(`It's a tie`);
     }
     else if (humanChoice == `rock` && computerChoice == `scissors`){
         console.log (`You win! Rock crushes scissors!`);
+        humanScore++
     }
     else if (humanChoice == `rock` && computerChoice == `paper`){
         console.log(`You LOSE! Paper covers rock`);
+        computerScore++
     }
     else if (humanChoice == `paper` && computerChoice == `rock`){
         console.log(`You win! Paper covers rock`);
+        humanScore++
     }
     else if (humanChoice == `paper` && computerChoice == `scissors`){
         console.log(`You lose! Scissors cuts paper`);
+        computerScore++
     }
     else if (humanChoice == `scissors` && computerChoice == `rock`){
         console.log(`You Loooooseeee, rock crushes scissors`);
+        computerScore++
     }
     else if (humanChoice == `scissors` && computerChoice == `paper`){
         console.log(`You win!! Scissors cuts up paper`)
+        humanScore++
     }
 }
 
-playRound()
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+console.log(`You: ${humanScore}`,`Computer: ${computerScore}`);
+

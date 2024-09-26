567858589d9070f2c7df1ecc7028b808ca89a692
Ridhcheall Doherty <132768875+hillister@users.noreply.github.com>
2024-09-26 14:19:05 +0100

    added prompt to get user choice of rock paper scissor and printed answer to the console

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 function getComputerchoice(){
     number = Math.floor((Math.random()) * 3) + 1
     if (number == 1){
-        console.log(`Rock`);
+        console.log(`rock`);
     }
     else if (number == 2){
-        console.log(`scissor`);
+        console.log(`scissors`);
     }
     else if (number == 3) {
         console.log(`paper`);
     }
 };
 
-getComputerchoice()
-
-
-/* fucntion getHumanChoice(){
-
-
+function getHumanChoice(){
+    answer = prompt(`choose rock paper or scissors: `)
+    if (answer == `scissors`){
+        console.log(answer);
+    }
+    else if (answer == `paper`){
+        console.log(answer);
+    }
+    else if (answer == `rock`){
+        console.log(answer);
+    }
+    else {
+        console.log(`That is not an option`)
+    }
 }
 
+console.log(`You chose:`);
+getHumanChoice()
 
-
-*/
+console.log(`Computer chose:`);
+getComputerchoice()

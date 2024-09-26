1d6bf22b509d768229dd2173a605ac6a9f88f5cf
Ridhcheall Doherty <132768875+hillister@users.noreply.github.com>
2024-09-26 14:04:00 +0100

    Added html and js files, started with the function to get ther computers choice of rock paper or scissor

---
+++ script.js
@@ -0,0 +1,24 @@
+function getComputerchoice(){
+    number = Math.floor((Math.random()) * 3) + 1
+    if (number == 1){
+        console.log(`Rock`);
+    }
+    else if (number == 2){
+        console.log(`scissor`);
+    }
+    else if (number == 3) {
+        console.log(`paper`);
+    }
+};
+
+getComputerchoice()
+
+
+/* fucntion getHumanChoice(){
+
+
+}
+
+
+
+*/

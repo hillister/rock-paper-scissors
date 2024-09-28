67dfeacaddbaab27ed934e7c63d9049586b66f0d
Ridhcheall Doherty <132768875+hillister@users.noreply.github.com>
2024-09-28 11:16:05 +0100

    added div to HTML DOM to keep score and show the outcome

--- script.js
+++ script.js
@@ -11,60 +11,69 @@ function getComputerChoice(){
     }
 };
 
+
 function playGame(){
     let humanScore = 0
     let computerScore = 0
     
     function playRound(humanChoice, computerChoice){
         if (humanChoice == `rock` && computerChoice == `rock`){
-            console.log(`It's a tie`);
+            score.textContent = `It's a tie`;
         }
         else if (humanChoice == `scissors` && computerChoice == `scissors`){
-            console.log(`It's a tie`);
+            score.textContent = `It's a tie`;
         }
         else if (humanChoice == `paper` && computerChoice == `paper`){
-            console.log(`It's a tie`);
+            score.textContent = `It's a tie`;
         }
         else if (humanChoice == `rock` && computerChoice == `scissors`){
-            console.log (`You win! Rock crushes scissors!`);
+            score.textContent = `You win! Rock crushes scissors!`;
             humanScore++
         }
         else if (humanChoice == `rock` && computerChoice == `paper`){
-            console.log(`You LOSE! Paper covers rock`);
+            score.textContent = `You LOSE! Paper covers rock`;
             computerScore++
         }
         else if (humanChoice == `paper` && computerChoice == `rock`){
-            console.log(`You win! Paper covers rock`);
+            score.textContent = `You win! Paper covers rock`;
             humanScore++
         }
         else if (humanChoice == `paper` && computerChoice == `scissors`){
-            console.log(`You lose! Scissors cuts paper`);
+            score.textContent = `You lose! Scissors cuts paper`;
             computerScore++
         }
         else if (humanChoice == `scissors` && computerChoice == `rock`){
-            console.log(`You Loooooseeee, rock crushes scissors`);
+            score.textContent = `You Loooooseeee, rock crushes scissors`;
             computerScore++
         }
         else if (humanChoice == `scissors` && computerChoice == `paper`){
-            console.log(`You win!! Scissors cuts up paper`)
+            score.textContent = `You win!! Scissors cuts up paper`;
             humanScore++
         }
     }
 
     document.getElementById("rock").addEventListener("click", function(){
         playRound('rock', getComputerChoice());
-        console.log(`You: ${humanScore}`,`Computer: ${computerScore}`);
+        count.textContent = `You: ${humanScore} Computer: ${computerScore}`;
     })
     document.getElementById("paper").addEventListener("click", function(){
         playRound('paper', getComputerChoice());
-        console.log(`You: ${humanScore}`,`Computer: ${computerScore}`);
+        count.textContent = `You: ${humanScore} Computer: ${computerScore}`;
     })
     document.getElementById("scissors").addEventListener("click", function(){
         playRound('scissors', getComputerChoice());
-        console.log(`You: ${humanScore}`,`Computer: ${computerScore}`);
+        count.textContent = `You: ${humanScore} Computer: ${computerScore}`;
     })
+
+    
 }
 
 playGame();
+
+let score = document.createElement("div");
+let count = document.createElement("div")
+let body = document.getElementsByTagName("body")[0];
+body.appendChild(score);
+body.appendChild(count);
 
 
